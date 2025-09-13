d91b7d013efae63aa6e2cf0683032e8415ae274d
Bahaa montaser <bebo18044@gmail.com>
2025-09-13 22:31:41 +0300

    corect loop code

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,13 +1,11 @@
 const arg1 = process.argv[2];
 const argValue = parseInt(arg1)
-
+let i = 0;
 if(Number.isInteger(argValue)){
-    for(let i = 0; i < argValue; i++){
-        console.log("C is fun");
-    }
+while (i < argValue) {
+  console.log("C is fun");
+  i++;
+}
 }else{
     console.log("Missing number of occurrences");
 }
-
-// typeof argValue === "number"
-// the problem was to make valur from  arg1 to number 
