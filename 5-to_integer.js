0cf3c4b4743fe4c209bccaa1f65f55a569e1b60e
Bahaa montaser <bebo18044@gmail.com>
2025-09-13 19:00:55 +0300

    add space in the code

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -2,9 +2,10 @@ const arg1 = process.argv[2];
 
 const int = parseInt(arg1)
 
+console.log(int)
 
 if(Number.isInteger(int)){
-    console.log("My number:"+ int) 
+    console.log("My number: "+ int) 
 }else {
     console.log("Not a number")
 }
