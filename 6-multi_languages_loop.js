dac6a15bbb90bdb7bf751136d9de9423a9532937
Bahaa montaser <bebo18044@gmail.com>
2025-09-13 21:17:01 +0300

    change for loop to while loop

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,6 +1,6 @@
-
 const arr = ["C is fun","Python is cool","JavaScript is amazing"];
-
-for(let i =0 ; arr.length > i ; i++){
-    console.log(arr[i]);
+let i = 0 ;
+while( arr.length > i){
+    console.log(arr[i])
+    i++;
 }
